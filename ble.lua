061f0bf9201a776f8ea39e54b67cb85d70a898a9
Michael Mogenson <michael.mogenson@gmail.com>
2025-02-09 21:10:27 -0500

    scan for peripherals

--- ble.lua
+++ ble.lua
@@ -21,18 +21,39 @@ local function NSString(str)
     return objc.NSString:stringWithUTF8String(str)
 end
 
+local function centralManagerDidUpdateState_(self, cmd, central)
+    local state = central.state
+    C.NSLog(NSString("central state %d"), state)
+    if (state == CBManagerStatePoweredOn) then
+        C.NSLog(NSString("central manager powered on, starting scan"))
+        local services = ffi.new("id") -- null pointer
+        local options = ffi.new("id")  -- null pointer
+        central_manager:scanForPeripheralsWithServices_options_(services, options)
+    end
+end
+
+local function centralManager_didDiscoverPeripheral_advertisementData_RSSI_(self, cmd, central, peripheral,
+                                                                            advertisement_data, rssi)
+    local ch8 = NSString("CH-8")
+    local name = peripheral.name
+    C.NSLog(NSString("discovered peripheral %@"), name)
+    -- if (name:isEqualToString_(ch8)) then
+    --     C.NSLog(NSString("found target peripheral"))
+    --     central_manager:stopScan()
+    -- end
+end
+
+local function makeDelegate()
+    local delegate_class = objc.newClass("CentralManagerDelegate")
+    objc.addMethod(delegate_class, "centralManagerDidUpdateState:", "v@:@", centralManagerDidUpdateState_)
+    objc.addMethod(delegate_class, "centralManager:didDiscoverPeripheral:advertisementData:RSSI:", "v@:@@@@",
+        centralManager_didDiscoverPeripheral_advertisementData_RSSI_)
+    return objc.CentralManagerDelegate:alloc():init()
+end
+
 
 local function main()
-    local delegate_class = objc.newClass("CentralManagerDelegate")
-    objc.addMethod(delegate_class, "centralManagerDidUpdateState:", "v@:@",
-        function(self, cmd, central)
-            local state = central.state
-            C.NSLog(NSString("central state %d"), state)
-            if (state == CBManagerStatePoweredOn) then
-                print("central manager powered on")
-            end
-        end)
-    local delegate = objc.CentralManagerDelegate:alloc():init()
+    local delegate = makeDelegate()
     local queue = ffi.cast("id", C._dispatch_main_q)
     central_manager = objc.CBCentralManager:alloc():initWithDelegate_queue_(delegate, queue)
 

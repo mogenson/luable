eaf0c636505e54da72067914bc7f0a5da0229414
Michael Mogenson <michael.mogenson@gmail.com>
2025-02-12 10:23:15 -0500

    connect to peripheral

--- ble.lua
+++ ble.lua
@@ -18,7 +18,10 @@ local CBManagerStatePoweredOn = ffi.new("NSInteger", 5)
 local YES = ffi.new("BOOL", 1)
 
 -- globals
-local central_manager = nil
+local App = {
+    delegate = nil,
+    peripheral = nil,
+}
 
 local function NSString(str)
     return objc.NSString:stringWithUTF8String(str)
@@ -26,25 +29,37 @@ end
 
 local function centralManagerDidUpdateState_(self, cmd, central)
     local state = central.state
-    C.NSLog(NSString("central state %d"), state)
+    C.NSLog(NSString("Central state %d"), state)
     if (state == CBManagerStatePoweredOn) then
-        C.NSLog(NSString("central manager powered on, starting scan"))
+        C.NSLog(NSString("Central manager powered on, starting scan"))
         local services = ffi.new("id") -- null pointer
         local options = ffi.new("id")  -- null pointer
-        central_manager:scanForPeripheralsWithServices_options_(services, options)
+        central:scanForPeripheralsWithServices_options_(services, options)
     end
 end
 
 local function centralManager_didDiscoverPeripheral_advertisementData_RSSI_(self, cmd, central, peripheral,
                                                                             advertisement_data, rssi)
     -- local ch8 = NSString("CH-8")
-    local ch8 = NSString("Govee_H6076_1959")
+    local ch8 = NSString("Foldy Boy")
     local name = peripheral.name
-    C.NSLog(NSString("discovered peripheral %@"), name)
+    C.NSLog(NSString("Discovered peripheral: %@"), name)
     if name and name:isEqualToString_(ch8) == YES then
-        C.NSLog(NSString("found %@, stopping scan"), ch8)
-        central_manager:stopScan()
+        C.NSLog(NSString("Matched name: %@, stopping scan and connecting"), ch8)
+        App.peripheral = peripheral:retain() -- connect will not succeed if peripheral is dropped
+        central:stopScan()
+        local options = ffi.new("id")        -- null pointer
+        central:connectPeripheral_options_(peripheral, options)
     end
+end
+
+local function centralManager_didConnectPeripheral_(self, cmd, central, peripheral)
+    C.NSLog(NSString("Connected to peripheral: %@"), peripheral.name)
+    peripheral.delegate = App.delegate
+end
+
+local function centralManager_didFailToConnectPeripheral_error_(self, cmd, centrl, peripheral, error)
+    C.NSLog(NSString("Failed to connect to peripheral: %@, Error: %@"), peripheral.name, error)
 end
 
 local function makeDelegate()
@@ -52,14 +67,17 @@ local function makeDelegate()
     objc.addMethod(delegate_class, "centralManagerDidUpdateState:", "v@:@", centralManagerDidUpdateState_)
     objc.addMethod(delegate_class, "centralManager:didDiscoverPeripheral:advertisementData:RSSI:", "v@:@@@@",
         centralManager_didDiscoverPeripheral_advertisementData_RSSI_)
+    objc.addMethod(delegate_class, "centralManager:didConnectPeripheral:", "v@:@@", centralManager_didConnectPeripheral_)
+    objc.addMethod(delegate_class, "centralManager:didFailToConnectPeripheral:error:", "v@:@@@",
+        centralManager_didFailToConnectPeripheral_error_)
     return objc.CentralManagerDelegate:alloc():init()
 end
 
 
 local function main()
-    local delegate = makeDelegate()
+    App.delegate = makeDelegate()
     local queue = ffi.cast("id", C._dispatch_main_q)
-    central_manager = objc.CBCentralManager:alloc():initWithDelegate_queue_(delegate, queue)
+    local central = objc.CBCentralManager:alloc():initWithDelegate_queue_(App.delegate, queue)
 
     C.CFRunLoopRun()
 end

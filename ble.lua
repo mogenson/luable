e819a0379edfffbae9bc2f44083a9445a66dd504
Michael Mogenson <michael.mogenson@gmail.com>
2025-02-17 10:38:40 -0500

    clean up ble.lua

--- ble.lua
+++ ble.lua
@@ -48,13 +48,16 @@ local function NSString(str)
     return objc.NSString:stringWithUTF8String(str)
 end
 
+local function NSLog(str, ...)
+    C.NSLog(NSString(str), ...)
+end
+
 -- constants
 local CBManagerStatePoweredOn = ffi.new("NSInteger", 5)
 local CBCharacteristicWriteWithoutResponse = ffi.new("NSInteger", 1)
 local YES = ffi.new("BOOL", 1)
 local MidiUuid = objc.CBUUID:UUIDWithString(NSString("7772E5DB-3868-4112-A1A9-F2669D106BF3"))
 local PeripheralName = NSString("CH-8")
---local PeripheralName = NSString("Foldy Boy")
 local MidiPacket = { 0x80, 0x80, 0x00, 0x00, 0x00 }
 
 -- globals
@@ -66,9 +69,9 @@ local App = {
 
 local function didUpdateState(self, cmd, central)
     local state = central.state
-    C.NSLog(NSString("Central state %d"), state)
+    NSLog("Central state %d", state)
     if (state == CBManagerStatePoweredOn) then
-        C.NSLog(NSString("Central manager powered on, starting scan"))
+        NSLog("Central manager powered on, starting scan")
         central:scanForPeripheralsWithServices_options(nil, nil)
     end
 end
@@ -76,9 +79,9 @@ end
 local function didDiscoverPeripheral(self, cmd, central, peripheral,
                                      advertisement_data, rssi)
     local name = peripheral.name
-    C.NSLog(NSString("Discovered peripheral: %@"), name)
+    NSLog("Discovered peripheral: %@", name)
     if name and name:isEqualToString(PeripheralName) == YES then
-        C.NSLog(NSString("Matched name: %@, stopping scan and connecting"), PeripheralName)
+        NSLog("Matched name: %@, stopping scan and connecting", PeripheralName)
         App.peripheral = peripheral:retain() -- connect will not succeed if peripheral is dropped
         central:stopScan()
         central:connectPeripheral_options(peripheral, nil)
@@ -86,49 +89,39 @@ local function didDiscoverPeripheral(self, cmd, central, peripheral,
 end
 
 local function didConnectPeripheral(self, cmd, central, peripheral)
-    C.NSLog(NSString("Connected to peripheral: %@"), peripheral.name)
+    NSLog("Connected to peripheral: %@", peripheral.name)
     peripheral.delegate = App.delegate
     peripheral:discoverServices(nil)
 end
 
-local function didDisconnectPeripheral(sefl, cmd, central, peripheral)
-    C.NSLog(NSString("Disconnected from peripheral: %@"), peripheral.name)
-    os.exit()
-end
-
-local function didFailToConnectPeripheral(self, cmd, central, peripheral, error)
-    C.NSLog(NSString("Failed to connect to peripheral: %@, Error: %@"), peripheral.name, error)
-    os.exit()
-end
-
 local function didDiscoverServices(self, cmd, peripheral, error)
     if objc.ptr(error) then
-        C.NSLog(NSString("Error discovering services: %@"), error)
+        NSLog("Error discovering services: %@", error)
         return
     end
 
-    C.NSLog(NSString("Discovered services:"))
+    NSLog("Discovered services:")
     local services = peripheral.services -- NSArray<CBService*>*
     for i = 0, tonumber(services.count) - 1 do
         local service = services:objectAtIndex(i)
-        C.NSLog(NSString("  %@"), service.UUID)
+        NSLog("  %@", service.UUID)
         peripheral:discoverCharacteristics_forService(nil, service)
     end
 end
 
 local function didDiscoverCharacteristics(self, cmd, peripheral, service, error)
     if objc.ptr(error) then
-        C.NSLog(NSString("Error discovering characteristics: %@"), error)
+        NSLog("Error discovering characteristics: %@", error)
         return
     end
 
-    C.NSLog(NSString("Discovered characteristics for service %@"), service.UUID)
+    NSLog("Discovered characteristics for service %@", service.UUID)
     local characteristics = service.characteristics
     for i = 0, tonumber(characteristics.count) - 1 do
         local characteristic = characteristics:objectAtIndex(i)
-        C.NSLog(NSString("  %@"), characteristic.UUID)
+        NSLog("  %@", characteristic.UUID)
         if characteristic.UUID:isEqual(MidiUuid) == YES then
-            C.NSLog(NSString("Found MIDI characteristic"))
+            NSLog("Found MIDI characteristic")
             App.characteristic = characteristic:retain()
             break
         end
@@ -141,9 +134,6 @@ local function makeDelegate()
     delegate_class:addMethod("centralManager:didDiscoverPeripheral:advertisementData:RSSI:", "v@:@@@@",
         didDiscoverPeripheral)
     delegate_class:addMethod("centralManager:didConnectPeripheral:", "v@:@@", didConnectPeripheral)
-    delegate_class:addMethod("centralManager:didDisconnectPeripheral:error:", "v@:@@@", didDisconnectPeripheral)
-    delegate_class:addMethod("centralManager:didFailToConnectPeripheral:error:", "v@:@@@",
-        didFailToConnectPeripheral)
     delegate_class:addMethod("peripheral:didDiscoverServices:", "v@:@@", didDiscoverServices)
     delegate_class:addMethod("peripheral:didDiscoverCharacteristicsForService:error:", "v@:@@@",
         didDiscoverCharacteristics)
@@ -153,7 +143,7 @@ end
 local function midi_input_callback(packet_list, ref_conn, conn)
     for i = 0, tonumber(packet_list.numPackets) - 1 do
         if tonumber(packet_list.packet[i].length) ~= 3 then
-            print("Expecting a Midi Packet with length of 3")
+            NSLog("Expecting a Midi Packet with length of 3")
             return
         end
 
@@ -165,10 +155,10 @@ local function midi_input_callback(packet_list, ref_conn, conn)
             ffi.cast("const void*", ffi.new("uint8_t[5]", MidiPacket)),
             ffi.new("NSUInteger", 5))
 
-        C.NSLog(NSString("MIDI data %@"), data)
+        NSLog("MIDI data %@", data)
 
         if App.peripheral and App.characteristic then
-            C.NSLog(NSString("Writing to characteristic %@"), App.characteristic)
+            NSLog("Writing to characteristic %@", App.characteristic)
             App.peripheral:writeValue_forCharacteristic_type(data, App.characteristic,
                 CBCharacteristicWriteWithoutResponse)
         end
@@ -177,16 +167,16 @@ end
 
 local function main()
     if tonumber(C.MIDIGetNumberOfSources()) == 0 then
-        print("No MIDI input sources")
+        NSLog("No MIDI input sources")
         return
     end
 
-    print("Creating MIDI client")
+    NSLog("Creating MIDI client")
     local midi_client = ffi.new("MIDIClientRef[1]", 0)
     local client_name = cf.CFString("LuaClient")
     assert(tonumber(C.MIDIClientCreate(client_name, nil, nil, midi_client)) == 0)
 
-    print("Creating MIDI input port")
+    NSLog("Creating MIDI input port")
     local midi_port = ffi.new("MIDIPortRef[1]", 0)
     local port_name = cf.CFString("LuaPort")
     local callback = ffi.cast("MIDIReadProc", midi_input_callback)
@@ -194,11 +184,11 @@ local function main()
 
     local midi_source = C.MIDIGetSource(0)
     if midi_source == 0 then
-        print("Invalid MIDI source")
+        NSLog("Invalid MIDI source")
         return
     end
 
-    print("Connecting MIDI source to MIDI port")
+    NSLog("Connecting MIDI source to MIDI port")
     assert(tonumber(C.MIDIPortConnectSource(midi_port[0], midi_source, nil)) == 0)
 
     App.delegate = makeDelegate()
--- main.lua
+++
@@ -1,4 +0,0 @@
-local cf = require("cf")
-
-local cfstr = cf.CFString("hello mike")
-cf.CFShow(cfstr)
